64f9ee7876ad9132b8bcae05d82584b08b201f8f
rbragaqueiroz <rbragadequeiroz@crimson.ua.edu>
2015-03-30 17:56:29 -0500

    Creating the Activitys
    Just creating the base pages

---
+++ BucketBuddy/app/src/main/java/com/cs495/bucketbuddy/GameScreenActivity.java
@@ -0,0 +1,18 @@
+package com.cs495.bucketbuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Rafael on 3/30/15.
+ */
+public class GameScreenActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamescreen);
+
+    }
+
+
+}
---
+++ BucketBuddy/app/src/main/java/com/cs495/bucketbuddy/TeamListActivity.java
@@ -0,0 +1,16 @@
+package com.cs495.bucketbuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Rafael on 3/30/15.
+ */
+public class TeamListActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teamlist );
+
+    }
+}
