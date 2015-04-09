320b9be007b040cfcdb75ea59b807a67ff5f0579
RobertHowe1 <rlhowe1@crimson.ua.edu>
2015-04-09 16:49:07 -0500

    Backbuttons & Radiobuttons
    Added backbuttons on the string of activities to create the team. Also
    added radiobuttons to the edit player activity, but there is no
    functionality to them yet.

--- BucketBuddy/app/src/main/java/com/cs495/bucketbuddy/Player.java
+++ BucketBuddy/app/src/main/java/com/cs495/bucketbuddy/Player.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
  */
 public class Player extends StatEntity {
 
+    public enum Position {PointGuard, ShootingGuard, SmallForward, PowerForward, Center};
     private long teamId;
 
     /**
